68a3f50cbaa3d5b6be15d7fdff51da2ae7771705
MussieBirhane <mussie.amaha@gmail.com>
2024-06-02 09:55:22 +0200

    '..'

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -10770,7 +10770,7 @@ var vapi = new _web.default("7388cbf1-3f35-45c5-abe6-f42a0abb12a9");
 document.getElementById('microphone-btn').addEventListener('click', function () {
   // Start the VAPI session with the persistent assistant's ID
   console.log('Microphone button clicked'); // Test log
-  vapi.start("9c246c0a-13c6-4b5b-8be2-151d9d64064e");
+  vapi.start("78a85399-9fcb-4dd3-8007-633a7f26da6d");
 
   // You can add event listeners if needed
   vapi.on('audioStart', function () {
