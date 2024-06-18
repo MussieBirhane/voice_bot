2f7a7e1a9cc81f7f541dc75a6093c5ec2808f5f5
MussieBirhane <mussie.amaha@gmail.com>
2024-06-18 22:57:19 +0200

    environmental variables

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 import Vapi from "@vapi-ai/web";
 
 // Create an instance of Vapi
-const vapi = new Vapi("7388cbf1-3f35-45c5-abe6-f42a0abb12a9");
+const vapi = new Vapi(process.env.VAPI_PUBLIC_KEY);
 
 // Set up the microphone button click event
 document.getElementById('microphone-btn').addEventListener('click', () => {
     // Start the VAPI session with the persistent assistant's ID
     console.log('Microphone button clicked');  // Test log
-    vapi.start("78a85399-9fcb-4dd3-8007-633a7f26da6d");   // 78a85399-9fcb-4dd3-8007-633a7f26da6d Cargo Trans
+    vapi.start(process.env.VAPI_ASSISTANT_ID);   // 78a85399-9fcb-4dd3-8007-633a7f26da6d Cargo Trans
 
     // New lines
     document.getElementById('microphone-btn').style.display = 'none';
